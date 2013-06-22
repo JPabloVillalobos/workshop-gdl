ff860d04f87a20eed2326b5d34d8be3e0baa5dd0
Juan Pablo Villalobos Usla <ing.juan.villalobos1990@gmail.com>
2013-06-22 10:52:57 -0500

    avances

--- workshop/credit-bureau/src/c/remplazar.c
+++ workshop/credit-bureau/src/c/remplazar.c
@@ -1,15 +1,24 @@
 #include<stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <winsock2.h>
+#include <unistd.h>
+#include <stdlib.h>
 //este programa busca el rfc y la fecha para poder cambiar el active 
 main()
 {
  FILE*fwriter;
  
+
  char buffer[256];
  char rfc[256]="23456XTEDG";
  char fecha[20]="30/15/2013";
- char active[2]="Y";
- char desactive[2]="N";
+ char Y[2]="Y";
+ char N[2]="N";
+ int cont1;
+ char caracter;
+
+ 
  
  fwriter=fopen("C:/Users/Usuario/Desktop/workshop/Loans2.txt", "r+");
 	if (fwriter == NULL)
@@ -18,12 +27,13 @@ main()
 	{
 		fgets(buffer,256,fwriter);
 		if ((strstr(buffer,rfc)) && (strstr(buffer,fecha))){
-			if(strrchr(buffer,active[2])){
-				fputc(desactive[2],fwriter);
-				printf("%s",buffer);
-				printf("RFC encontrado");
-				break;
-			}
+				if (printf((caracter = fgetc(fwriter))) == Y[2]){ 
+					fputc(N[2], fwriter);
+					printf("RFC encontrado y loan cancelado");
+					break;
+				
+				}
+
 		}
 	}
  	fclose (fwriter);
